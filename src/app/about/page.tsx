3e3461b339b3035498ff14008c75d977d8d56ff5
Alexander Köhler <xelak@outlook.de>
2023-03-23 16:01:56 +0100

    create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 mt-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our IMDB clone! We are passionate about movies and TV shows,
+        and we created this platform to share our love for cinema with you.
+      </p>
+      <p>
+        Our team consists of movie enthusiasts, who strive to provide you with
+        the best possible user experience. We aim to be your go-to source for
+        all things movies and TV shows, from the latest blockbusters to hidden
+        gems you may have missed.
+      </p>
+      <p>
+        Our platform features a vast collection of movies and TV shows, with
+        detailed information about each title, including synopsis, cast, crew,
+        and reviews. You can browse our extensive library, search for specific
+        titles, and even create your watchlist to keep track of the movies and
+        shows you want to watch.
+      </p>
+      <p>
+        We also provide a platform for our users to share their thoughts and
+        opinions on movies and TV shows. You can rate and review titles, create
+        lists, and engage with other users through our community features. We
+        believe that sharing your passion for movies with others is an essential
+        part of the cinema experience.
+      </p>
+      <p>
+        We are committed to making our platform accessible and easy to use for
+        everyone. We strive to provide a seamless user experience across all
+        devices, whether you're on your desktop or mobile device. Our platform
+        is constantly evolving, and we're always looking for new ways to improve
+        and enhance your experience.
+      </p>
+      <p>
+        Thank you for visiting our IMDB clone, and we hope you enjoy your time
+        here! If you have any feedback or suggestions, please don't hesitate to
+        reach out to us. We're always happy to hear from our users and improve
+        our platform.
+      </p>
+    </div>
+  );
+}
